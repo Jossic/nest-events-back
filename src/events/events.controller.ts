c01ee2aeca90ce447dc1d74a95e7e130fb2908cc
Jossic <jossic.lapierre@gmail.com>
2022-11-24 16:52:21 +0100

    Associating related entities

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -13,6 +13,7 @@ import {
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { MoreThan, Repository } from 'typeorm';
+import { Attendee } from './attendee.entity';
 import { CreateEventDto } from './create-event.dto';
 import { Event } from './event.entity';
 import { UpdateEventDto } from './update-event.dto';
@@ -23,31 +24,49 @@ export class EventsController {
 
   constructor(
     @InjectRepository(Event)
-    private readonly repository: Repository<Event>,
+    private readonly eventRepository: Repository<Event>,
+    @InjectRepository(Attendee)
+    private readonly attendeeRepository: Repository<Attendee>,
   ) {}
 
   @Get()
   async findAll() {
-    return await this.repository.find();
+    return await this.eventRepository.find();
   }
 
   @Get('/practice')
   async Practice() {
-    return await this.repository.find({ where: { id: MoreThan(2) } });
+    return await this.eventRepository.find({ where: { id: MoreThan(2) } });
   }
   @Get('/practice2')
   async Practice2() {
-    return await this.repository.findOne({
+    const event = await this.eventRepository.findOne({
       where: { id: 1 },
-      relations: {
-        attendees: true,
-      },
+      relations: { attendees: true },
     });
+    if (!event) {
+      throw new NotFoundException();
+    }
+    const attendee = new Attendee();
+    attendee.name = 'Jerry';
+    attendee.event = event;
+    await this.attendeeRepository.save({ ...attendee });
+
+    // With Cascade - set cascade to true in oneToMany relation to event entity
+    // const attendee = new Attendee();
+    // attendee.name = 'Jerry with cascade';
+    // event.attendees.push(attendee);
+    // await this.eventRepository.save({ ...event });
+
+    return event;
   }
 
   @Get(':id')
   async findOne(@Param('id', ParseIntPipe) id: number) {
-    const event = await this.repository.findOneBy({ id });
+    const event = await this.eventRepository.findOne({
+      where: { id },
+      relations: { attendees: true },
+    });
     if (!event) {
       throw new NotFoundException();
     }
@@ -56,7 +75,7 @@ export class EventsController {
 
   @Post()
   async create(@Body() input: CreateEventDto) {
-    return await this.repository.save({
+    return await this.eventRepository.save({
       ...input,
       when: new Date(input.when),
     });
@@ -64,11 +83,11 @@ export class EventsController {
 
   @Patch(':id')
   async update(@Param('id') id, @Body() input: UpdateEventDto) {
-    const event = await this.repository.findOneBy({ id });
+    const event = await this.eventRepository.findOneBy({ id });
     if (!event) {
       throw new NotFoundException();
     }
-    return await this.repository.save({
+    return await this.eventRepository.save({
       ...event,
       ...input,
       when: input.when ? new Date(input.when) : event.when,
@@ -78,10 +97,10 @@ export class EventsController {
   @Delete(':id')
   @HttpCode(204)
   async remove(@Param('id') id) {
-    const event = await this.repository.findOneBy({ id });
+    const event = await this.eventRepository.findOneBy({ id });
     if (!event) {
       throw new NotFoundException();
     }
-    await this.repository.remove(event);
+    await this.eventRepository.remove(event);
   }
 }
