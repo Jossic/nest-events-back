af17d3855961252901496248084bcea60e076d88
Jossic <jossic.lapierre@gmail.com>
2022-11-24 10:17:22 +0100

    Add controls to Dto

--- src/events.controller.ts
+++ src/events.controller.ts
@@ -7,6 +7,7 @@ import {
   Param,
   Patch,
   Post,
+  ValidationPipe,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { MoreThan, Repository } from 'typeorm';
