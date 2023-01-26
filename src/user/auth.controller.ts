9a904008083e5ea4e0660ed990c417755393e5a6
Jossic <jossic.lapierre@gmail.com>
2023-01-26 14:28:52 +0100

    feat: setup JWT

--- src/user/auth.controller.ts
+++ src/user/auth.controller.ts
@@ -1,14 +1,17 @@
 import { Controller, Post, UseGuards, Request } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
   @Post('login')
   @UseGuards(AuthGuard('local'))
   async login(@Request() request) {
     return {
       userId: request.user.id,
-      token: 'toooken',
+      token: this.authService.getTokenForUser(request.user),
     };
   }
 }
